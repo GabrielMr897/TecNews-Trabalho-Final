08e25e1832b4e796231dd0dc43485cbd458216f3
Gabriel Martins <gabriel-pms@hotmail.com>
2022-09-27 17:10:49 -0300

    novas funcionalidades na página principal

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
-function switchTheme() {
-    const botao = document.querySelector("#flexSwitchCheckDefault")
+function darkMode() {
+  document.body.classList.add("darkMode");
+}
 
-    if(botao.checked){
-        document.body.style.backgroundColor = '#000'
-        document.body.style.color = '#fff'
-    } else {
-        document.body.style.backgroundColor = '#fff'
-        document.body.style.color = '#000'
-    }
+function lightMode() {
+  document.body.classList.remove("darkMode");
 }
