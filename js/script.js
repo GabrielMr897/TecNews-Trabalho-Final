8a2f9a4682a2949f4acb489ceaf54c02a08cdd88
Gabriel Martins <gabriel-pms@hotmail.com>
2022-09-28 22:01:20 -0300

    Validações e etc

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ function lightMode() {
   document.body.classList.remove("darkMode");
 }
 
+const contatos = [];
+
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("envia").addEventListener("click", function (sub) {
     let nome = document.getElementById("nome").value; //Carregamos o valor do campo nome
@@ -30,13 +32,26 @@ document.addEventListener("DOMContentLoaded", function () {
       validacoes++;
     }
 
-    console.log(validacoes);
-
     if (validacoes != 4) {
       sub.preventDefault();
       alert("Preencha corretamente todos os campos");
     } else {
-      window.location.href = "contato.html";
+      localStorage.nome = nome;
+      localStorage.email = email;
+      localStorage.nascimento = nascimento;
+      localStorage.mensagem = mensagem;
+
+      console.log(
+        localStorage.nome,
+        localStorage.email,
+        localStorage.nascimento,
+        localStorage.mensagem
+      );
+
+      document.getElementById("nome").value = "";
+      document.getElementById("email1").value = "";
+      document.getElementById("nascimento").value = "";
+      document.getElementById("msg").value = "";
     }
   });
 });
